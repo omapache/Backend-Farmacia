d0cbe67aeb70b0fd914897f277b0138a74fdf9f4
omapache <owenblanco77@gmail.com>
2023-09-22 01:45:55 -0500

    feat: :sparkles: segunda parte que se me olvido darle en stage changes

--- Dominio/Interfaces/IUser.cs
+++ Dominio/Interfaces/IUser.cs
@@ -1,7 +1,7 @@
 using Dominio.Entities;
 
 namespace Dominio.Interfaces;
-public interface IUserRepository : IGenericRepo<User>
+public interface IUser : IGenericRepo<User>
 { 
     Task<User> GetByUsernameAsync(string username);
     Task<User> GetByRefreshTokenAsync(string username);
